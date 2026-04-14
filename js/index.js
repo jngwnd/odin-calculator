f77017e28c853855ef2f3a612062da398e8df4c5
vosien <202865739+vosien@users.noreply.github.com>
2026-04-14 04:06:45 -0500

    feat: register number and basic operator button functionalities

--- js/index.js
+++ js/index.js
@@ -1,13 +1,69 @@
-let aNum = 0;
-let bNum = 0;
-let operator = '';
-
+const display = document.querySelector('#display');
 const buttons = document.querySelector('#buttons');
-const numbers = [...document.querySelectorAll('.number')];
+
+let currNum = '';
+let numArr = [];
+let operators = [];
 
 buttons.addEventListener('click', (e) => {
-    
+    const target = e.target;
+    const value = target.dataset.value;
+
+    const digit = /^\d$/;
+    const operator = /[+\-*/]/;
+
+    if (digit.test(value)) {
+        processNum(target);
+    } else if (operator.test(value)) {
+        processOp(target);
+    } else if (value === '=') {
+        processEq(target);
+    }
 })
+
+function processNum (target) {
+    currNum += target.dataset.value;
+    display.textContent = (display.textContent === '0') ? target.dataset.value : display.textContent + target.dataset.value;
+    console.log(currNum);
+}
+
+function processOp (target) {
+    const value = target.dataset.value;
+
+    if (value === '-') {
+        if (display.textContent !== '0') {
+            saveToArr(value, currNum);
+        } else {
+            currNum += value;
+        }
+        display.textContent = (display.textContent == '0') ? value : display.textContent + value;
+    } else {
+        saveToArr(value, currNum);
+        display.textContent += value;
+    }
+    console.log(currNum, numArr, operators);
+}
+
+function processEq () {
+    numArr.push(currNum);
+    const aNum = Number(numArr[0]);
+    const bNum = Number(numArr[1]);
+    const op = operators[0];
+
+    console.log(aNum, bNum, op);
+
+    numArr.length = 0;
+    operators.shift();
+    currNum = `${operate(aNum, bNum, op)}`;
+    display.textContent = currNum;
+    console.log(numArr);
+}
+
+function saveToArr(val, num) {
+    numArr.push(num);
+    operators.push(val);
+    currNum = '';
+}
 
 function add(a, b) {
     return a + b;
@@ -25,15 +81,15 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(aNum, operator, bNum) {
+function operate(a, b, operator) {
     switch (operator) {
         case '+':
-            return add(aNum, bNum);
+            return add(a, b);
         case '-':
-            return subtract(aNum, bNum);
+            return subtract(a, b);
         case '*':
-            return multiply(aNum, bNum);
+            return multiply(a, b);
         case '/':
-            return divide(aNum, bNum);
+            return divide(a, b);
     }
 }
