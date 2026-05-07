5cbc3855bef8568842db92f450d4d60da08ea29e
vosien <202865739+vosien@users.noreply.github.com>
2026-05-07 02:19:25 -0500

    feat: add equality / modulo and streamline operators

--- js/index.js
+++ js/index.js
@@ -1,105 +1,112 @@
+const mathjs = window.math;
+
 const display = document.querySelector('#display');
 const buttons = document.querySelector('#buttons');
+const operator = /[+\-*/]/;
 
-let currNum = '';
-let numArr = [];
-let operators = [];
+
+let currNum = '0';
+let prevBtn = '';
+
+console.log(mathjs.evaluate("5%"));
 
 buttons.addEventListener('click', (e) => {
     const target = e.target;
     const value = target.dataset.value;
 
     const digit = /^\d$/;
-    const operator = /[+\-*/]/;
 
-    if (digit.test(value)) {
-        processNum(target);
-    } else if (operator.test(value)) {
-        processOp(target);
-    } else if (value === '=') {
-        processEq(target);
-    }
+    if (digit.test(value)) processNum(target);
+    if (operator.test(value)) processOp(target);
+    if (value === '=') processEq(target);
+    if (value === '%') processMod(target);
+
+    display.textContent = display.dataset.value;
+    console.log(`curr: ${currNum}`, `dis: ${display.dataset.value}`);
 })
 
-function processNum (target) {
-    currNum += target.dataset.value;
-    display.textContent = (display.textContent === '0') ? target.dataset.value : display.textContent + target.dataset.value;
-    console.log(currNum);
-}
 
-function processOp (target) {
+/* 
+STEP 1: Concatenate target value to currNum
+STEP 2: Set display value to target value if display value equals 0, otherwise appends target value to display value
+
+* needs to replace 0 with positive int if 0 is the currNum after operator
+*/
+function processNum(target) {
     const value = target.dataset.value;
 
-    if (value === '-') {
-        if (display.textContent !== '0') {
-            saveToArr(value, currNum);
-            if (numArr.length === 2) {
-                processEq();
-            }
-        } else {
-            currNum += value;
-        }
-        display.textContent = (display.textContent == '0') ? value : display.textContent + value;
-    } else {
-        saveToArr(value, currNum);
-        if (numArr.length === 2) {
-            processEq();
-        }
-        display.textContent += value;
+    if (prevBtn === '=') {
+        display.dataset.value = '';
+        currNum = '0';
     }
-    console.log(currNum, numArr, operators);
+    currNum = (currNum === '0') ? value : (currNum + value);
+    display.dataset.value = (display.dataset.value === '0') ? value : (display.dataset.value + value);
 }
 
-function processEq () {
-    numArr.push(currNum);
-    const aNum = Number(numArr[0]);
-    const bNum = Number(numArr[1]);
-    const op = operators[0];
-
-    console.log(aNum, bNum, op);
-
-    numArr.length = 0;
-    operators.shift();
-    numArr.push(`${operate(aNum, bNum, op)}`);
-    currNum = '';
-    display.textContent = numArr[0];
-    console.log(numArr);
-}
-
-function saveToArr(val, num) {
-    numArr.push(num);
-    operators.push(val);
-    currNum = '';
-}
-
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(a, b, operator) {
-    if ([a, b, operator].includes(undefined)) {
-        return currNum;
-    }
-    switch (operator) {
-        case '+':
-            return add(a, b);
+function processOp(target) {
+    const value = target.dataset.value;
+    const addSub = /[+\-]/;
+    
+    switch (value) {
         case '-':
-            return subtract(a, b);
+            if (addSub.test(display.dataset.value.at(-1))) {
+                display.dataset.value = display.dataset.value.slice(0, -1) + value;
+            } else if (display.dataset.value === '0') {
+                display.dataset.value = value;
+            } else {
+                display.dataset.value += value;
+            }
+            break;
+
+        case '+':
         case '*':
-            return multiply(a, b);
         case '/':
-            return divide(a, b);
+            if (display.dataset.value.at(-1) === '-' && operator.test(display.dataset.value.at(-2))) {
+                display.dataset.value = display.dataset.value.slice(0, -2) + value;
+            } else if (operator.test(display.dataset.value.at(-1))) {
+                display.dataset.value = display.dataset.value.slice(0, -1) + value;
+            } else {
+                display.dataset.value += value;
+            }
+            break;
+
     }
+    currNum = '';
+}
+
+function processEq(target) {
+    display.dataset.value = currNum = mathjs.evaluate(display.dataset.value);
+    prevBtn = target.dataset.value;
+}
+
+function processMod(target) {
+    const value = target.dataset.value;
+
+    if (operator.test(display.dataset.value.at(-1))) {
+        if (display.dataset.value.at(-2) === '%') {
+            display.dataset.value = display.dataset.value.slice(0, -1);
+            return;
+        }
+        display.dataset.value = display.dataset.value.slice(0, -1) + value;
+
+    } else if (display.dataset.value.at(-1) === '%') {
+        if (currNum === '') {
+            const lastOpIndex = lastRegexIndex(display.dataset.value, operator);
+            console.log(lastOpIndex);
+            currNum = display.dataset.value.slice(lastOpIndex, -1) + '%';
+        }
+        currNum = `(${currNum})%`;
+        display.dataset.value = display.dataset.value.slice(0, -(currNum.length - 3)) + currNum;
+
+    } else {
+        currNum += '%';
+        display.dataset.value += '%';
+    }
+}
+
+function lastRegexIndex(str, regex) {
+    const gRegex = new RegExp(regex.source, regex.flags.includes('g') ? regex.flags : regex.flags + 'g');
+    const matches = [...str.matchAll(gRegex)];
+    console.log(matches);
+    return matches.length ? matches.at(-2).index : 0;
 }
