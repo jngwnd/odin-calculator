b1eaa5b8c2883d27f800fa98c0e31ed86abca620
vosien <202865739+vosien@users.noreply.github.com>
2026-05-10 23:38:53 -0500

    feat: add decimal, sign, delete, and clear

--- js/index.js
+++ js/index.js
@@ -2,27 +2,31 @@ const mathjs = window.math;
 
 const display = document.querySelector('#display');
 const buttons = document.querySelector('#buttons');
+const clear = document.querySelector('#clear');
+const digit = /\d/;
 const operator = /[+\-*/]/;
 
 
 let currNum = '0';
 let prevBtn = '';
 
-console.log(mathjs.evaluate("5%"));
+console.log(`curr: ${currNum}`, `dis: ${display.dataset.value}`, `prev: ${prevBtn}`);
 
 buttons.addEventListener('click', (e) => {
     const target = e.target;
     const value = target.dataset.value;
 
-    const digit = /^\d$/;
-
     if (digit.test(value)) processNum(target);
     if (operator.test(value)) processOp(target);
     if (value === '=') processEq(target);
     if (value === '%') processMod(target);
+    if (value === '.') processDec(target);
+    if (value === 'delete') processDelete();
+    if (value === 'clear') processClear();
+    if (value === 'sign') processSign();
 
     display.textContent = display.dataset.value;
-    console.log(`curr: ${currNum}`, `dis: ${display.dataset.value}`);
+    console.log(`curr: ${currNum}`, `dis: ${display.dataset.value}`, `prev: ${prevBtn}`);
 })
 
 
@@ -39,8 +43,16 @@ function processNum(target) {
         display.dataset.value = '';
         currNum = '0';
     }
+    if (display.dataset.value.at(-1) === '%') {
+        currNum = '0';
+    }
+    if (display.dataset.value.at(-1) === ')') {
+        display.dataset.value += '*';
+    }
     currNum = (currNum === '0') ? value : (currNum + value);
     display.dataset.value = (display.dataset.value === '0') ? value : (display.dataset.value + value);
+    prevBtn = value;
+    clear.textContent = 'C';
 }
 
 function processOp(target) {
@@ -49,6 +61,8 @@ function processOp(target) {
     
     switch (value) {
         case '-':
+            currNum = (currNum === '0' && display.dataset.value === '0') ? value : '';
+            
             if (addSub.test(display.dataset.value.at(-1))) {
                 display.dataset.value = display.dataset.value.slice(0, -1) + value;
             } else if (display.dataset.value === '0') {
@@ -61,6 +75,10 @@ function processOp(target) {
         case '+':
         case '*':
         case '/':
+            if (display.dataset.value.at(-1) === '(') {
+                return;
+            }
+            
             if (display.dataset.value.at(-1) === '-' && operator.test(display.dataset.value.at(-2))) {
                 display.dataset.value = display.dataset.value.slice(0, -2) + value;
             } else if (operator.test(display.dataset.value.at(-1))) {
@@ -68,32 +86,47 @@ function processOp(target) {
             } else {
                 display.dataset.value += value;
             }
+            currNum = '';
             break;
 
     }
-    currNum = '';
+    prevBtn = value;
+    clear.textContent = 'C';
 }
 
 function processEq(target) {
-    display.dataset.value = currNum = mathjs.evaluate(display.dataset.value);
+    if (operator.test(display.dataset.value.at(-1)) || display.dataset.value.at(-1) === '(') {
+        return;
+    }
+    const result = mathjs.evaluate(display.dataset.value);
+    currNum = parseFloat(result.toFixed(10)).toString();
+    display.dataset.value = (currNum === 'Infinity') ? "Undefined" : currNum;
     prevBtn = target.dataset.value;
+    clear.textContent = 'AC';
 }
 
 function processMod(target) {
     const value = target.dataset.value;
 
+    if (display.dataset.value.at(-1) === '(') {
+        return;
+    }
+
     if (operator.test(display.dataset.value.at(-1))) {
         if (display.dataset.value.at(-2) === '%') {
             display.dataset.value = display.dataset.value.slice(0, -1);
+            const opIndex = lastRegIndex(display.dataset.value, operator);
+            currNum = display.dataset.value.slice(opIndex + 1, -1) + '%';
             return;
         }
         display.dataset.value = display.dataset.value.slice(0, -1) + value;
+        const opIndex = lastRegIndex(display.dataset.value, operator);
+        currNum = display.dataset.value.slice(opIndex + 1, -1) + '%';
 
     } else if (display.dataset.value.at(-1) === '%') {
         if (currNum === '') {
-            const lastOpIndex = lastRegexIndex(display.dataset.value, operator);
-            console.log(lastOpIndex);
-            currNum = display.dataset.value.slice(lastOpIndex, -1) + '%';
+            const opIndex = lastRegIndex(display.dataset.value, operator);
+            currNum = display.dataset.value.slice(opIndex + 1, -1) + '%';
         }
         currNum = `(${currNum})%`;
         display.dataset.value = display.dataset.value.slice(0, -(currNum.length - 3)) + currNum;
@@ -102,11 +135,108 @@ function processMod(target) {
         currNum += '%';
         display.dataset.value += '%';
     }
+    prevBtn = value;
+    clear.textContent = 'C';
 }
 
-function lastRegexIndex(str, regex) {
+function processDec(target) {
+    const value = target.dataset.value;
+
+    if (currNum.includes('.')) {
+        return;
+    } else if (currNum === '') {
+        currNum = '0.';
+        display.dataset.value += currNum;
+    } else {
+        currNum += '.';
+        display.dataset.value += value;
+    }
+    prevBtn = value;
+    clear.textContent = 'C';
+}
+
+function processSign() {
+    let opIndex;
+
+    if (currNum === '' ||
+        currNum === '0' ||
+        display.dataset.value.at(-1) === '(') {
+        return;
+    }
+
+    if (/^\d+\.?\d*%?$/.test(currNum)) {
+        opIndex = (display.dataset.value.at(-1) === '%') ? lastRegIndex(display.dataset.value, operator) :
+                                                           lastRegIndex(display.dataset.value, /[+\-*/%]/);
+        currNum = `(-${currNum})`;
+
+    } else if (/^\(-\d+\.?\d*%?\)$/.test(currNum)) {
+        opIndex = (display.dataset.value.at(-1) === '%') ? lastRegIndex(display.dataset.value, operator, 2) :
+                                                           lastRegIndex(display.dataset.value, /[+\-*/%]/, 2);
+        currNum = currNum.slice(2, -1);
+    } else if (/^-\d+\.?\d*%?$/.test(currNum)) {
+        opIndex = (display.dataset.value.at(-1) === '%') ? lastRegIndex(display.dataset.value, operator, 2) :
+                                                           lastRegIndex(display.dataset.value, /[+\-*/%]/, 2);
+        currNum = currNum.slice(-(currNum.length - 1));                                                                
+    }
+
+    display.dataset.value = display.dataset.value.slice(0, opIndex + 1) + currNum;
+}
+
+function processDelete() {
+    const digDec = /[\d$.]/;
+
+    if (display.dataset.value === "Undefined") {
+        return;
+    }
+    
+    if (display.dataset.value.at(-1) === ')') {
+        const parenIndex = lastRegIndex(display.dataset.value, /\(/);
+        if (display.dataset.value.at(parenIndex - 1) === '+' && display.dataset.value.at(parenIndex + 1) === '-') {
+            display.dataset.value = display.dataset.value.slice(0, parenIndex - 1) +
+                                    display.dataset.value.slice((display.dataset.value.length - (parenIndex + 1)), -1);
+        } else {
+            display.dataset.value = display.dataset.value.slice(0, parenIndex) +
+                                    display.dataset.value.slice((display.dataset.value.length - (parenIndex + 1)), -1);
+        }
+    } else {
+        display.dataset.value = display.dataset.value.slice(0, -1);
+    }
+    if (display.dataset.value === '') {
+        currNum = '0';
+        display.dataset.value = '0';
+    }
+    if (operator.test(display.dataset.value)) {
+        currNum = '';
+    } else if (digDec.test(display.dataset.value.at(-1))) {
+        const opIndex = lastRegIndex(display.dataset.value, /[+\-*/%]/);
+        currNum = display.dataset.value.slice(-(display.dataset.value.length - opIndex));
+    } else if (currNum) {
+        currNum.slice(0, -1);
+    }
+
+}
+
+function processClear() {
+    const clearContent = clear.textContent;
+    
+    if (clearContent === 'AC') {
+        currNum = '0';
+        display.dataset.value = '0';
+    } else if (clearContent === 'C') {
+        const opIndex = lastRegIndex(display.dataset.value, /[+\-*/%]/);
+
+        currNum = '';
+        display.dataset.value = display.dataset.value.slice(0, opIndex + 1);
+        if (display.dataset.value === '') {
+            currNum = '0';
+            display.dataset.value = '0';
+        }
+        clear.textContent = 'AC';        
+    }
+}
+
+function lastRegIndex(str, regex, negIndex = 1) {
     const gRegex = new RegExp(regex.source, regex.flags.includes('g') ? regex.flags : regex.flags + 'g');
     const matches = [...str.matchAll(gRegex)];
-    console.log(matches);
-    return matches.length ? matches.at(-2).index : 0;
+    return (matches.length >= negIndex) ? matches.at(-negIndex).index : -1;
 }
